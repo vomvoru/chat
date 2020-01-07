b15dd3663c4ee0ceccecca1f3435504169ad3d0e
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2020-01-07 13:08:26 +0900

    방 떠나기 버튼 텍스트 수정

--- packages/client/src/view/Pages/RoomPage/LeaveButton/index.tsx
+++ packages/client/src/view/Pages/RoomPage/LeaveButton/index.tsx
@@ -5,5 +5,5 @@ import { useLeaveRoom } from '../../../../hooks/useLeaveRoom';
 export const LeaveButton: FC = () => {
   const leave = useLeaveRoom();
 
-  return <input type="button" value="leave" onClick={leave} />;
+  return <input type="button" value="leave room" onClick={leave} />;
 };

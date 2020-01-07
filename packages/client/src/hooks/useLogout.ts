69e2048e9f2ad513a6aa9ebdc9fd105f0920384f
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2020-01-07 13:07:31 +0900

    로그아웃시 방도 떠나도록 로직 개선

--- packages/client/src/hooks/useLogout.ts
+++ packages/client/src/hooks/useLogout.ts
@@ -1,13 +1,18 @@
 import { useContext, useCallback } from 'react';
 
 import { SetUserContext } from './contexts/User';
+import { useLeaveRoom } from './useLeaveRoom';
 
 export const useLogout = () => {
   const setUser = useContext(SetUserContext);
+  const leave = useLeaveRoom();
 
   const logout = useCallback(() => {
-    if (setUser) setUser(null);
-  }, [setUser]);
+    if (setUser) {
+      setUser(null);
+      leave();
+    }
+  }, [leave, setUser]);
 
   return logout;
 };
